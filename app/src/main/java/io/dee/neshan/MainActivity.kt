bd361541a0b24e493b6fe9d89c6a0439846606b1
Danial Iranmehr <danidaniran6@gmail.com>
2024-07-29 13:45:18 +0330

    - add text view - rebase

--- app/src/main/java/io/dee/neshan/MainActivity.kt
+++ app/src/main/java/io/dee/neshan/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.dee.neshan
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.tvText)
+
     }
 }
